d5bde1884f2909b366d6bae6adeb20221b871dcf
Ben Buhse <ben@buhses.com>
2020-10-01 12:32:52 -0500

    Add basic routing for the about page and model page
    Theoretically, the instance routing should be there too but I haven't created a template for it yet

--- app.py
+++ app.py
@@ -5,7 +5,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
-	app.run(debug=True)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/model/<string:model>')
+def model(model=None):
+    '''
+    Return a model's page.
+
+    <model> is one of: player, team or nationality, or match
+    '''
+    # TODO: Load data from the DB
+    # TODO: 404 if the model does not exist
+    return render_template('model.html', model=model)
+
+
+@app.route('/instance/<string:model>/<int:id>')
+def instance(model=None, instance=0):
+    '''
+    Return a instance's page.
+
+    <model> is one of: player, team or nationality, or match
+    <instance> is the integer id of the specific instance
+    '''
+    # TODO: Load data from the DB
+    # TODO: 404 if the model or id do not exist
+    return render_template('instance.html', model=model, instance=instance)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
