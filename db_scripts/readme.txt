d0d112f66a533c24011ccffff9c528c0351d0684
Juan Paez <juanpaez@utexas.edu>
2020-10-17 01:21:57 -0500

    throttling, cleaner scraping, filter players

--- db_scripts/readme.txt
+++ db_scripts/readme.txt
@@ -1,11 +1,19 @@
 Scripts to initialize ChampionsDB database to different extents.
 
-Basic Database (phase 1):
+Initialize Basic Database (phase 1):
 1. init_basic_db.py: hardcodes 10 instances into database
 2. append_data_db.py: adds more data to the hardcoded instances
 
-Full Database (phase 2-4):
+Initialize Full Database (phase 2-4):
 1. scrape_matches.py: scrapes matches from API-Football
 2. scrape_teams.py: scrapes teams from API-Football and Twitter
 3. scrape_player_events.py: scrapes player events (goals, cards, substitutes) from API-Football
 4. coalesce_players.py: coalesces player events into players and brings in data from FutDB
+
+Notes:
+1. Some matches pre-group stage did not have info available about players/scorers.
+    - these matches/teams were ignored and not included in our counts
+2. Players only include match captains and goal scorers
+    - would otherwise have thousands of players-- API quotas make this impossible
+3. These scripts try to get as much information with as few queries due to quotas.
+    - much of the scraping had to be done incrementally due to exceeding quotas
--- db_scripts/scrape_player_events.py
+++ db_scripts/scrape_player_events.py
@@ -7,6 +7,7 @@ import json
 import pymongo
 from pymongo import MongoClient
 import config
+import time
 
 def main():
     # Get players from DB. Does NOT clear old, unlike other scraping scripts.
@@ -23,8 +24,8 @@ def main():
     # ************************************* IMPORTANT *************************************
     # Make sure to set this limit BEFORE running this script, or API might charge absurdly.
 
-    call_limit = 20     # API call limit
-    start = 0           # Match index at which we left off last time
+    call_limit = 80     # API call limit
+    start = 160         # Match index at which we left off last time
 
     events = []
     match_ids = match_ids[start:start + call_limit]
@@ -40,11 +41,14 @@ def main():
     for match_id in match_ids:
         try:
             call_count += 1
+            time.sleep(3)
+            print(f"Request #{call_count}     Match ID={match_id}")
             response = requests.get(f"{api_football_uri}/players/fixture/{match_id}", headers=api_football_headers).json()
             match_events = response["api"]["players"]
             for event in match_events:
                 event["match_id"] = match_id  # Add match id to the event
             events = events + match_events
+            print(f"Received {len(match_events)} events!")
         except:
             print(f"WARNING: failed after {call_count} calls at match_id {match_id}. Proceeding")
             call_count -= 1
@@ -53,30 +57,38 @@ def main():
     
     # Clean event information
     for event in events:
-        event["_id"] = event.pop("event_id", None) * 100000000 + event["player_id"] # Janky way to produce unique id
-        event.pop("updateAt", None)
+        try:
+            event["_id"] = event.pop("event_id", None) * 100000000 + event["player_id"] # Janky way to produce unique id
+            event.pop("updateAt", None)
 
-        shots = event.pop("shots", None)
-        event["shots"] = shots["total"]
-        event["shots_on_target"] = shots["on"]
+            shots = event.pop("shots", None)
+            event["shots"] = shots["total"]
+            event["shots_on_target"] = shots["on"]
 
-        goals = event.pop("goals", None)
-        event["goals"] = goals["total"]
-        event["assists"] = goals["assists"]
+            goals = event.pop("goals", None)
+            event["goals"] = goals["total"]
+            event["assists"] = goals["assists"]
 
-        passes = event.pop("passes", None)
-        event["passes"] = passes["total"]
-        event["pass_accuracy"] = passes["accuracy"]
+            passes = event.pop("passes", None)
+            event["passes"] = passes["total"]
+            event["pass_accuracy"] = passes["accuracy"]
 
-        event.pop("tackles")
-        event.pop("duels")
-        event.pop("dribbles")
-        event.pop("fouls")
-        event.pop("penalty")
-        event.pop("cards")
+            event.pop("tackles")
+            event.pop("duels")
+            event.pop("dribbles")
+            event.pop("fouls")
+            event.pop("penalty")
+            event.pop("cards")
+        except:
+            print('WARNING: failed to clean an event.')
+            print(event)
+            continue
 
     # Store events back to DB
-    event_collection.insert_many(events)
+    try:
+        event_collection.insert_many(events)
+    except:
+        event_collection.insert_many(events, {'ordered': False})
     print("Event count: {}".format(event_collection.count_documents({})))
     print("NOTE: Next starting index-- " + str(start + call_count))
 
